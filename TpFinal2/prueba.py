c8cb94d1269044ca0689278982caa1699f09890b
AlejandroM <alejandro.maglio@ing.austral.edu.ar>
2021-12-18 18:59:31 -0300

    TPFinal Parte 2

---
+++ TpFinal2/prueba.py
@@ -0,0 +1,304 @@
+import os
+import shutil
+
+import matplotlib.pyplot as plt
+import nibabel as nib
+import numpy as np
+from PIL import Image, ImageDraw
+from scipy.spatial import ConvexHull
+from skimage import measure
+
+import glob
+import csv
+
+
+def make_dirs(path):
+    """
+    Creates the directory as specified from the path
+    in case it exists it deletes it
+    """
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.makedirs(path)
+
+def intensity_seg(ct_numpy, min, max):
+    clipped = ct_numpy.clip(min, max)
+    clipped[clipped != max] = 1
+    clipped[clipped == max] = 0
+    return measure.find_contours(clipped, 0.95)
+
+def set_is_closed(contour):
+    if contour_distance(contour) < 1:
+        return True
+    else:
+        return False
+
+def find_lungs(contours):
+    """
+    Chooses the contours that correspond to the lungs and the body
+    FIrst we exclude non closed sets-contours
+    Then we assume some min area and volume to exclude small contours
+    Then the body is excluded as the highest volume closed set
+    The remaining areas correspond to the lungs
+
+    Args:
+        contours: all the detected contours
+
+    Returns: contours that correspond to the lung area
+
+    """
+    body_and_lung_contours = []
+    vol_contours = []
+
+    for contour in contours:
+        hull = ConvexHull(contour)
+
+        if hull.volume > 2000 and set_is_closed(contour):
+            body_and_lung_contours.append(contour)
+            vol_contours.append(hull.volume)
+
+    if len(body_and_lung_contours) == 2:
+        return body_and_lung_contours
+    elif len(body_and_lung_contours) > 2:
+        vol_contours, body_and_lung_contours = (list(t) for t in
+                                                zip(*sorted(zip(vol_contours, body_and_lung_contours))))
+        body_and_lung_contours.pop(-1)
+        return body_and_lung_contours
+
+
+def show_contour(image, contours, name=None, save=False):
+    fig, ax = plt.subplots()
+    ax.imshow(image.T, cmap=plt.cm.gray)
+    for contour in contours:
+        ax.plot(contour[:, 0], contour[:, 1], linewidth=1)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    if save:
+        plt.savefig(name)
+        plt.close(fig)
+    else:
+        plt.show()
+
+
+
+def create_mask_from_polygon(image, contours):
+
+    lung_mask = np.array(Image.new('L', image.shape, 0))
+    for contour in contours:
+        x = contour[:, 0]
+        y = contour[:, 1]
+        polygon_tuple = list(zip(x, y))
+        img = Image.new('L', image.shape, 0)
+        ImageDraw.Draw(img).polygon(polygon_tuple, outline=0, fill=1)
+        mask = np.array(img)
+        lung_mask += mask
+
+    lung_mask[lung_mask > 1] = 1  # sanity check to make 100% sure that the mask is binary
+
+    return lung_mask.T  # transpose it to be aligned with the image dims
+
+def save_nifty(img_np, name, affine):
+
+    img_np[img_np == 1] = 255
+    ni_img = nib.Nifti1Image(img_np, affine)
+    nib.save(ni_img, name + '.nii.gz')
+
+
+def find_pix_dim(ct_img):
+    pix_dim = ct_img.header["pixdim"]
+    dim = ct_img.header["dim"]
+    max_indx = np.argmax(dim)
+    pixdimX = pix_dim[max_indx]
+    dim = np.delete(dim, max_indx)
+    pix_dim = np.delete(pix_dim, max_indx)
+    max_indy = np.argmax(dim)
+    pixdimY = pix_dim[max_indy]
+    return [pixdimX, pixdimY]
+
+def contour_distance(contour):
+
+    dx = contour[0, 1] - contour[-1, 1]
+    dy = contour[0, 0] - contour[-1, 0]
+    return np.sqrt(np.power(dx, 2) + np.power(dy, 2))
+
+def compute_area(mask, pixdim):
+    mask[mask >= 1] = 1
+    lung_pixels = np.sum(mask)
+    return lung_pixels * pixdim[0] * pixdim[1]
+
+
+
+
+
+def show_slice(slice):
+
+    plt.figure()
+    plt.imshow(slice.T, cmap="gray", origin="lower")
+
+def show_slice_window(slice, level, window):
+
+    max = level + window/2
+    min = level - window/2
+    slice = slice.clip(min,max)
+    plt.figure()
+    plt.imshow(slice.T, cmap="gray", origin="lower")
+    plt.savefig('L'+str(level)+'W'+str(window))
+
+
+def overlay_plot(im, mask):
+    plt.figure()
+    plt.imshow(im.T, 'gray', interpolation='none')
+    plt.imshow(mask.T, 'jet', interpolation='none', alpha=0.5)
+
+
+
+basepath = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/Images/slice*.nii.gz'
+paths = sorted(glob.glob(basepath))
+print('Images found:', len(paths))
+
+for c, exam_path in enumerate(paths):
+    ct_img = nib.load(exam_path)
+    ct_numpy = ct_img.get_fdata()
+
+    if c == 1:
+      show_slice(ct_numpy)
+      plt.savefig('original')
+      show_slice_window(ct_numpy,+50,350)
+      show_slice_window(ct_numpy,-200,2000)
+      break
+
+
+outpath = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/LUNGS/'
+contour_path = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/Contours/'
+paths = sorted(glob.glob(basepath))
+myFile = open('lung_volumes.csv', 'w')
+lung_areas = []
+make_dirs(outpath)
+make_dirs(contour_path)
+
+for c, exam_path in enumerate(paths):
+    img_name = exam_path.split("/")[-1].split('.nii')[0]
+    img_name= img_name.split('\\')[-1]
+    out_mask_name = outpath + img_name + "_mask"
+    contour_name = contour_path + img_name + "_contour"
+
+    ct_img = nib.load(exam_path)
+    pixdim = find_pix_dim(ct_img)
+    ct_numpy = ct_img.get_fdata()
+
+    contours = intensity_seg(ct_numpy, min=-1000, max=-300)
+
+    lungs = find_lungs(contours)
+    show_contour(ct_numpy, lungs, contour_name,save=True)
+    lung_mask = create_mask_from_polygon(ct_numpy, lungs)
+    save_nifty(lung_mask, out_mask_name, ct_img.affine)
+    show_slice(lung_mask)
+
+    lung_area = compute_area(lung_mask, find_pix_dim(ct_img))
+    lung_areas.append([img_name,lung_area]) # int is ok since the units are already mm^2
+    print(img_name,'lung area:', lung_area)
+
+
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(lung_areas)
+
+
+
+vessels = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/Vessels/'
+figures = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/Figures/'
+overlay_path = 'C:/Users/aleja/PycharmProjects/Grupo6-VisionArt/TPFinal2/Vessel_overlayed/'
+paths = sorted(glob.glob(basepath))
+myFile = open('vessel_volumes.csv', 'w')
+lung_areas_csv = []
+ratios = []
+
+make_dirs(vessels)
+make_dirs(overlay_path)
+make_dirs(figures)
+
+def euclidean_dist(dx, dy):
+    return np.sqrt(np.power(dx, 2) + np.power(dy, 2))
+
+def denoise_vessels(lung_contour, vessels):
+    vessels_coords_x, vessels_coords_y = np.nonzero(vessels)  # get non zero coordinates
+    for contour in lung_contour:
+        x_points, y_points = contour[:, 0], contour[:, 1]
+        for (coord_x, coord_y) in zip(vessels_coords_x, vessels_coords_y):
+            for (x, y) in zip(x_points, y_points):
+                d = euclidean_dist(x - coord_x, y - coord_y)
+                if d <= 0.1:
+                    vessels[coord_x, coord_y] = 0
+    return vessels
+
+
+def split_array_coords(array, indx=0, indy=1):
+    x = [array[i][indx] for i in range(len(array))]
+    y = [array[i][indy] for i in range(len(array))]
+    return x, y
+
+
+def create_vessel_mask(lung_mask, ct_numpy, denoise=False):
+    vessels = lung_mask * ct_numpy  # isolate lung area
+    vessels[vessels == 0] = -1000
+    vessels[vessels >= -500] = 1
+    vessels[vessels < -500] = 0
+    show_slice(vessels)
+    if denoise:
+        return denoise_vessels(lungs_contour, vessels)
+    show_slice(vessels)
+
+    return vessels
+
+
+for c, exam_path in enumerate(paths):
+    img_name = exam_path.split("/")[-1].split('.nii')[0] #images/slice00n.nii.gz
+    img_name = exam_path.split("\\")[-1].split('.nii.gz')[0]  #slice00n
+    vessel_name = vessels + img_name + "_vessel_only_mask.nii.gz"            # + '.nii.gz
+    overlay_name = overlay_path + img_name + "_vessels.png"
+
+    ct_img = nib.load(exam_path)
+    pixdim = find_pix_dim(ct_img)
+    ct_numpy = ct_img.get_fdata()
+
+    contours = intensity_seg(ct_numpy, -1000, -300)
+
+    lungs_contour = find_lungs(contours)
+    lung_mask = create_mask_from_polygon(ct_numpy, lungs_contour)
+
+    lung_area = compute_area(lung_mask, find_pix_dim(ct_img))
+
+    vessels_only = create_vessel_mask(lung_mask, ct_numpy, denoise=True)
+
+    overlay_plot(ct_numpy, vessels_only)
+    plt.title('Overlayed plot')
+    plt.savefig(overlay_name)  #no funca
+    plt.close()
+
+    save_nifty(vessels_only, vessel_name, affine=ct_img.affine)
+
+    vessel_area = compute_area(vessels_only, find_pix_dim(ct_img))
+    ratio = (vessel_area / lung_area) * 100
+    print(img_name, 'Vessel %:', ratio)
+    lung_areas_csv.append([img_name, lung_area, vessel_area, ratio])
+    ratios.append(ratio)
+
+# Save data to csv file
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(lung_areas_csv)
+
+
+
+
+
+
+
+
+
+
+
--- TpFinal2/pruebaN.py
+++ TpFinal2/pruebaN.py
@@ -23,11 +23,13 @@ def make_dirs(path):
     else:
         os.makedirs(path)
 
+
 def intensity_seg(ct_numpy, min, max):
     clipped = ct_numpy.clip(min, max)
     clipped[clipped != max] = 1
     clipped[clipped == max] = 0
     return measure.find_contours(clipped, 0.95)
+
 
 def set_is_closed(contour):
     if contour_distance(contour) < 1:
@@ -35,10 +37,11 @@ def set_is_closed(contour):
     else:
         return False
 
+
 def find_lungs(contours):
     """
     Chooses the contours that correspond to the lungs and the body
-    FIrst we exclude non closed sets-contours
+    First we exclude non closed sets-contours
     Then we assume some min area and volume to exclude small contours
     Then the body is excluded as the highest volume closed set
     The remaining areas correspond to the lungs
@@ -84,7 +87,6 @@ def show_contour(image, contours, name=None, save=False):
         plt.show()
 
 
-
 def create_mask_from_polygon(image, contours):
     """
     Creates a binary mask with the dimensions of the image and
@@ -110,6 +112,7 @@ def create_mask_from_polygon(image, contours):
     lung_mask[lung_mask > 1] = 1  # sanity check to make 100% sure that the mask is binary
 
     return lung_mask.T  # transpose it to be aligned with the image dims
+
 
 def save_nifty(img_np, name, affine):
     """
@@ -147,6 +150,7 @@ def find_pix_dim(ct_img):
     pixdimY = pix_dim[max_indy]
     return [pixdimX, pixdimY]
 
+
 def contour_distance(contour):
     """
     Given a set of points that may describe a contour
@@ -160,6 +164,7 @@ def contour_distance(contour):
     dx = contour[0, 1] - contour[-1, 1]
     dy = contour[0, 0] - contour[-1, 0]
     return np.sqrt(np.power(dx, 2) + np.power(dy, 2))
+
 
 def compute_area(mask, pixdim):
     """
@@ -176,9 +181,6 @@ def compute_area(mask, pixdim):
     return lung_pixels * pixdim[0] * pixdim[1]
 
 
-
-
-
 def show_slice(slice):
     """
     Function to display an image slice
@@ -187,6 +189,7 @@ def show_slice(slice):
     plt.figure()
     plt.imshow(slice.T, cmap="gray", origin="lower")
 
+
 def show_slice_window(slice, level, window):
     """
     Function to display an image slice
@@ -194,7 +197,7 @@ def show_slice_window(slice, level, window):
     """
     max = level + window/2
     min = level - window/2
-    slice = slice.clip(min,max)
+    slice = slice.clip(min, max)
     plt.figure()
     plt.imshow(slice.T, cmap="gray", origin="lower")
     plt.savefig('L'+str(level)+'W'+str(window))
@@ -204,7 +207,6 @@ def overlay_plot(im, mask):
     plt.figure()
     plt.imshow(im.T, 'gray', interpolation='none')
     plt.imshow(mask.T, 'jet', interpolation='none', alpha=0.5)
-
 
 
 basepath = 'C:/Users/aleja/PycharmProjects/TPFinal2/Images/slice*.nii.gz'
@@ -218,8 +220,8 @@ for c, exam_path in enumerate(paths):
     if c == 1:
       show_slice(ct_numpy)
       plt.savefig('original')
-      show_slice_window(ct_numpy,+50,350)
-      show_slice_window(ct_numpy,-200,2000)
+      show_slice_window(ct_numpy, +50, 350)
+      show_slice_window(ct_numpy, -200, 2000)
       break
 
 
@@ -233,7 +235,7 @@ make_dirs(contour_path)
 
 for c, exam_path in enumerate(paths):
     img_name = exam_path.split("/")[-1].split('.nii')[0]
-    img_name= img_name.split('\\')[-1]
+    img_name = img_name.split('\\')[-1]
     out_mask_name = outpath + img_name + "_mask"
     contour_name = contour_path + img_name + "_contour"
 
@@ -244,20 +246,19 @@ for c, exam_path in enumerate(paths):
     contours = intensity_seg(ct_numpy, min=-1000, max=-300)
 
     lungs = find_lungs(contours)
-    show_contour(ct_numpy, lungs, contour_name,save=True)
+    show_contour(ct_numpy, lungs, contour_name, save=True)
     lung_mask = create_mask_from_polygon(ct_numpy, lungs)
     save_nifty(lung_mask, out_mask_name, ct_img.affine)
     show_slice(lung_mask)
 
     lung_area = compute_area(lung_mask, find_pix_dim(ct_img))
-    lung_areas.append([img_name,lung_area]) # int is ok since the units are already mm^2
-    print(img_name,'lung area:', lung_area)
+    lung_areas.append([img_name, lung_area])  # int is ok since the units are already mm^2
+    print(img_name, 'lung area:', lung_area)
 
 
 with myFile:
     writer = csv.writer(myFile)
     writer.writerows(lung_areas)
-
 
 
 vessels = 'C:/Users/aleja/PycharmProjects/TPFinal2/Vessels/'
@@ -272,8 +273,10 @@ make_dirs(vessels)
 make_dirs(overlay_path)
 make_dirs(figures)
 
+
 def euclidean_dist(dx, dy):
     return np.sqrt(np.power(dx, 2) + np.power(dy, 2))
+
 
 def denoise_vessels(lung_contour, vessels):
     vessels_coords_x, vessels_coords_y = np.nonzero(vessels)  # get non zero coordinates
@@ -307,8 +310,8 @@ def create_vessel_mask(lung_mask, ct_numpy, denoise=False):
 
 
 for c, exam_path in enumerate(paths):
-    img_name = exam_path.split("/")[-1].split('.nii')[0] #images/slice00n.nii.gz
-    img_name = exam_path.split("\\")[-1].split('.nii.gz')[0]  #slice00n
+    img_name = exam_path.split("/")[-1].split('.nii')[0]  # images/slice00n.nii.gz
+    img_name = exam_path.split("\\")[-1].split('.nii.gz')[0]  # slice00n
     vessel_name = vessels + img_name + "_vessel_only_mask.nii.gz"            # + '.nii.gz
     overlay_name = overlay_path + img_name + "_vessels.png"
 
@@ -327,7 +330,7 @@ for c, exam_path in enumerate(paths):
 
     overlay_plot(ct_numpy, vessels_only)
     plt.title('Overlayed plot')
-    plt.savefig(overlay_name)  #no funca
+    plt.savefig(overlay_name)
     plt.close()
 
     save_nifty(vessels_only, vessel_name, affine=ct_img.affine)
@@ -342,14 +345,3 @@ for c, exam_path in enumerate(paths):
 with myFile:
     writer = csv.writer(myFile)
     writer.writerows(lung_areas_csv)
-
-
-
-
-
-
-
-
-
-
-
